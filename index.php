981133d6566233109a2498c167323e0a51fb2ae3
ZedLicense <zedlicense@gmail.com>
2023-10-29 21:36:47 +0330

    Create index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>ZedLicense System</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link href="https://fonts.googleapis.com/css?family=Righteous" rel="stylesheet">
+<link rel="stylesheet" href="/style.css">
+</head>
+<body>
+<div class="maindiv">
+<h1 class="text">
+ZedLicense System
+</h1>
+</div>
+</body>
+</html>
